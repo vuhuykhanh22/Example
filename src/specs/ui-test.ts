ab19c35fbea97b6e6e3be88c335b354a76ae76ed
vuhuykhanh22 <37165580+vuhuykhanh22@users.noreply.github.com>
2020-12-09 08:58:12 +0700

    Update ui-test.ts

--- src/specs/ui-test.ts
+++ src/specs/ui-test.ts
@@ -6,13 +6,12 @@ describe('Index', () => {
     browser.$("div.form-username-slash input[name='username']").setValue(`${process.env.USERNAME}`);
     browser.$("div.form-password-slash input[name='password']").setValue(`${process.env.PASSWORD}`);
     browser.$('input.login-button').click();
-    browser.pause(8000);
+    browser.pause(10000);
     // const localSettingForDisableWelcomeInfo = 'window.localStorage.setItem("gaia.1::com.cybozu.kintone.mobile.LocalSetting", \'{"v2NavigationPanelButtonTooltipDisplayed":true,"v2WelcomeDialogDisplayed":true}\')';
     // browser.executeScript(localSettingForDisableWelcomeInfo, [])
     browser.$('button.gaia-mobile-v2-ui-welcomedialog-close-button').click();
     browser.pause(5000)
     browser.$('button.gaia-mobile-v2-ui-tooltip-closebutton').click();
-    browser.pause(2000)
   });
 
   it('should save some screenshots', () => {
