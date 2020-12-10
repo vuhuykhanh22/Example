9ac0adc8181c07c01ae156d34b74833dea1df274
Vu Huy Khanh <k001534@PC-001534-1.local>
2020-12-10 09:34:58 +0700

    Upload

--- wdio.conf.js
+++ wdio.conf.js
@@ -159,7 +159,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 60000,
+    timeout: 180000,
     require: 'ts-node/register',
     compilers: [
       // optional
