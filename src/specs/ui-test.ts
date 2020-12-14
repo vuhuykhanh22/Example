6b0875804bb551182e8e01aa6f47e876c686901b
Vu Huy Khanh <k001534@PC-001534-1.local>
2020-12-14 14:09:06 +0700

    Upload

--- src/specs/ui-test.ts
+++ src/specs/ui-test.ts
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 
+const appID = 717;
 describe('Index', () => {
   before(() => {
-    browser.url(`https://${process.env.DOMAIN}/k/m/717/`);
+    browser.url(`https://${process.env.DOMAIN}/k/m/${appID}`);
     browser.$("div.form-username-slash input[name='username']").setValue(`${process.env.USERNAME}`);
     browser.$("div.form-password-slash input[name='password']").setValue(`${process.env.PASSWORD}`);
     browser.$('input.login-button').click();
@@ -25,7 +26,7 @@ describe('Index', () => {
 
   it('should compare successful with a baseline', () => {
     // Check a full page screenshot
-    expect(browser.checkFullPageScreen('index-fullPage', {})).toEqual(0);
+    expect(browser.checkFullPageScreen('index-fullPage', {})).to.equal(0);
 
     // // Check a full page screenshot with all tab executions
     // expect(browser.checkTabbablePage('index-tabbable', { /* some options, use the same options as for checkFullPageScreen */})).toEqual(0);
@@ -48,7 +49,7 @@ describe('Create', () => {
 
   it('should compare successful with a baseline', () => {
       browser.pause(5000);
-      expect(browser.checkFullPageScreen('Create-fullPage', { })).toEqual(0);
+      expect(browser.checkFullPageScreen('Create-fullPage', { })).to.equal(0);
       browser.pause(5000);
 
       // expect(browser.checkTabbablePage('Create-tabbable', { /* some options, use the same options as for checkFullPageScreen */ })).toEqual(0);
@@ -70,7 +71,7 @@ describe('Detail', () => {
 
   it('should compare successful with a baseline', () => {
       browser.pause(5000);
-      expect(browser.checkFullPageScreen('Detail-fullPage', { })).toEqual(0);
+      expect(browser.checkFullPageScreen('Detail-fullPage', { })).to.equal(0);
 
       // expect(browser.checkTabbablePage('Detail-tabbable', { /* some options, use the same options as for checkFullPageScreen */ })).toEqual(0);
   });
@@ -89,7 +90,7 @@ describe('Edit', () => {
   });
 
   it('should compare successful with a baseline', () => {
-      expect(browser.checkFullPageScreen('Edit-fullPage', { })).toEqual(0);
+      expect(browser.checkFullPageScreen('Edit-fullPage', { })).to.equal(0);
 
       // expect(browser.checkTabbablePage('Edit-tabbable', { /* some options, use the same options as for checkFullPageScreen */ })).toEqual(0);
   });
